761e18261072446e07f21bde6b92e536e0b059df
iRainna <caihonglin@joomon.com>
2024-07-17 17:48:26 +0800

    feat: r-sidebar demo

---
+++ pages/example/sidebar/sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <view> </view>
+</template>
+
+<script setup></script>
---
+++ uni_modules/r-sidebar/components/r-sidebar/r-sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <view :class="{ 'van-sidebar': true }">
+    <slot></slot>
+  </view>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.van-sidebar {
+  width: var(--van-sidebar-width);
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+</style>
