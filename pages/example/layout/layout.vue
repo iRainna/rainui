742c8939c71519c01164ed6e776278c7fd889730
iRainna <caihonglin@joomon.com>
2024-07-26 18:01:33 +0800

    fix:增加示例

--- pages/example/layout/layout.vue
+++ pages/example/layout/layout.vue
@@ -1,22 +1,282 @@
 <template>
-	<view>
-		layout
-	</view>
+  <r-config-provider>
+    <view style="padding: 20rpx 0">
+      <r-divider content-position="left">基本使用--示例1</r-divider>
+      <r-row>
+        <r-col style="background-color: #39a9ed; text-align: center" span="8">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+        <r-col style="background-color: #66c6f2; text-align: center" span="8">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+        <r-col style="background-color: #39a9ed; text-align: center" span="8">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+      </r-row>
+      <r-divider content-position="left">基本使用--示例2</r-divider>
+      <r-row>
+        <r-col span="4">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 4
+          </view>
+        </r-col>
+        <r-col span="10" offset="4">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            offset: 4, span: 10
+          </view>
+        </r-col>
+      </r-row>
+      <r-divider content-position="left">基本使用--示例3</r-divider>
+      <r-row>
+        <r-col offset="12" span="12">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            offset: 12, span: 12
+          </view>
+        </r-col>
+      </r-row>
+
+      <r-divider content-position="left">设置列元素间距</r-divider>
+
+      <r-row gutter="20rpx">
+        <r-col span="8">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+        <r-col span="8">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+        <r-col span="8">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 8
+          </view>
+        </r-col>
+      </r-row>
+
+      <r-divider content-position="left">对齐方式--居中</r-divider>
+      <r-row justify="center">
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+      </r-row>
+
+      <r-divider content-position="left">对齐方式--右对齐</r-divider>
+
+      <r-row justify="flex-end">
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+      </r-row>
+
+      <r-divider content-position="left">对齐方式--两端对齐</r-divider>
+      <r-row justify="space-between">
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+      </r-row>
+
+      <r-divider content-position="left"
+        >对齐方式--每个元素的两侧间隔相等</r-divider
+      >
+
+      <r-row justify="space-around">
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #66c6f2;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+        <r-col span="6">
+          <view
+            style="
+              background-color: #39a9ed;
+              text-align: center;
+              padding: 20rpx 0;
+            "
+          >
+            span: 6
+          </view>
+        </r-col>
+      </r-row>
+    </view>
+  </r-config-provider>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- pages/example/popup/popup.vue
+++ pages/example/popup/popup.vue
@@ -1,22 +1,133 @@
 <template>
-	<view>
-		
-	</view>
+  <r-config-provider>
+    <view style="padding: 20rpx">
+      <r-divider content-position="left">弹出位置 </r-divider>
+      <r-cell-group>
+        <r-cell
+          is-link
+          title="顶部弹出"
+          @click="
+            open({
+              position: 'top',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="底部弹出"
+          @click="
+            open({
+              position: 'bottom',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="左侧弹出"
+          @click="
+            open({
+              position: 'left',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="右侧弹出"
+          @click="
+            open({
+              position: 'right',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="居中弹出"
+          @click="
+            open({
+              position: 'center',
+            })
+          "
+        />
+      </r-cell-group>
+
+      <r-divider content-position="left">关闭图标 </r-divider>
+
+      <r-cell-group>
+        <r-cell
+          is-link
+          title="关闭图标"
+          @click="
+            open({
+              closeable: true,
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="自定义图标"
+          @click="
+            open({
+              closeable: true,
+              closeIcon: 'close',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="图标位置"
+          @click="
+            open({
+              closeable: true,
+              closeIconPosition: 'top-left',
+            })
+          "
+        />
+      </r-cell-group>
+
+      <r-divider content-position="left">圆角弹窗 </r-divider>
+      <r-cell-group>
+        <r-cell
+          is-link
+          title="圆角弹窗（居中）"
+          @click="
+            open({
+              round: true,
+              position: 'center',
+            })
+          "
+        />
+        <r-cell
+          is-link
+          title="圆角弹窗（底部）"
+          @click="
+            open({
+              round: true,
+              position: 'bottom',
+            })
+          "
+        />
+      </r-cell-group>
+    </view>
+  </r-config-provider>
+  <r-popup
+    v-model:show="show"
+    :round="popupParams.round"
+    :position="popupParams.position || 'bottom'"
+    :closeable="popupParams.closeable"
+    :close-icon="popupParams.closeIcon || 'cross'"
+    :close-icon-position="popupParams.closeIconPosition || 'top-right'"
+    @click-close-icon="show = false"
+  >
+    <view style="height: 100px; background-color: #fff">11111</view>
+  </r-popup>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+import { ref } from "vue";
+const show = ref(false);
+const popupParams = ref({});
+const open = (e) => {
+  popupParams.value = e;
+  show.value = true;
+};
 </script>
-
-<style>
-
-</style>
