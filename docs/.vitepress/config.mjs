47d3f511b94473c8c809605b9e5ca265bb36073b
iRainna <caihonglin@joomon.com>
2024-08-07 16:21:24 +0800

    fix:修改文档

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -100,7 +100,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: "github", link: "https://github.com/iRainna/rainui" },
     ],
   },
 });
