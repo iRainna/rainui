084c932b59126f05e2e906596496181d42ba3efd
iRainna <caihonglin@joomon.com>
2024-08-07 17:19:51 +0800

    fix:修改文档

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Rainui",
+  lastUpdated: true,
   description: "使用uniapp-vue3的ui库",
   base: "/rainui/",
   themeConfig: {
@@ -12,6 +13,9 @@ export default defineConfig({
       { text: "组件", link: "/guide/install" },
     ],
     logo: "/logo.jpg",
+    search: {
+      provider: "local",
+    },
 
     sidebar: [
       {
@@ -101,6 +105,22 @@ export default defineConfig({
 
     socialLinks: [
       { icon: "github", link: "https://github.com/iRainna/rainui" },
+      {
+        icon: {
+          svg: '<svg t="1704626282666" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4227" width="200" height="200"><path d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z" fill="#C71D23" p-id="4228"></path></svg>',
+        },
+        link: "https://gitee.com/uv86e/rainui",
+      },
+      {
+        icon: {
+          svg: `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16" height="16" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve">  <image id="image0" width="16" height="16" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACGUlEQVQ4jX2TXYgPYRTGfzu7KN8T0bJ7haYUtcooIqFeM9m40EZqXG1t2im1Co1Lbyjc/CW1d1OyF1z4aN6dkjYbaaREbbZ8tlplafN1gVidnNW/Je/NTO85z/Oe8zznNDDlhHESAp3ARuCHRhuBQaC3KvKqHvGHIIyTucAFoAU4BdysivyrxmYA24DDwGugqyryjxLz6sD9wIOqyDf5aXDPT4N2iRlnF/tpsMtPg9sSkxzJVcxvAn35SlXkZ4yze4BnwGmNLQT6gBHj7F7JAS4rBk97blGw9H0RmAfMUYIv+pW7c8bZ1qrIzwJLwzhZ56lgJ42z04A7QLu2M6bA98BT4DiwrIyyEb0XnTobwjgZAtr8NNiv4O4yyl4ZZ0X5ReJEGWVvVY/ZCrw2XhseAB5KBROq9mpgBzBknN0JrABeAs+Ns83G2bXAY+AAsFUxP5uEYEqv8tooMAuYDjTp/xKtSM5M/U54KqT4fAs4D6wqo+x+GWVi13pxGXhRRtlVYCVwA3gUxomQe6JBr1rYr33OB3oAsXNNGWWfjLNPhASoqcCN47XhLcBuqUAIjii4VWfgGLAcWFA3C9uB68CGMsq+A0cF6+lsj4ZxclAt6gY+KFB6l/NZ7/aVUTYouYIR7OQkdgEdSnIJkEqkhXcaP6Qz0KfgDsX8c5magRPAQFXk3zQmgm3Wst/UL9P/1lkcmLRY8u7+tc7AL5BhyngPO/ubAAAAAElFTkSuQmCC" ></image></svg>`,
+        },
+        link: "https://ext.dcloud.net.cn/plugin?id=19701",
+      },
     ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright (c) 2024 iRainna",
+    },
   },
 });
