de2ef1a7bd99268baa25fc1fff44c8f5a1deef22
iRainna <caihonglin@joomon.com>
2025-10-31 16:43:25 +0800

    fix: r-progress

--- uni_modules/r-progress/components/r-progress/r-progress.vue
+++ uni_modules/r-progress/components/r-progress/r-progress.vue
@@ -30,11 +30,10 @@
   </view>
 </template>
 <script setup>
-import { getComponentThemeCssVar } from "@/uni_modules/r-theme/js_sdk/index.js";
-import {
-  CONFIG_PROVIDER_KEY,
-  isNumeric,
-} from "@/uni_modules/r-utils/js_sdk/index.js";
+import { getComponentThemeCssVar } from "@/uni_modules/r-theme-base/js_sdk/useComponentTheme.js";
+import { isNumeric } from "@/uni_modules/r-utils-basic/js_sdk/index.js";
+import { CONFIG_PROVIDER_KEY } from "@/uni_modules/r-utils-constant/js_sdk/index.js";
+import { datas } from "../themes/index.js";
 import { inject, computed } from "vue";
 const props = defineProps({
   percentage: {
@@ -88,8 +87,8 @@ const getComponentThemeStyle = computed(() => {
   }
 
   return {
-    ...getComponentThemeCssVar(themeName, "r-base"),
-    ...getComponentThemeCssVar(themeName, componentsName),
+    ...getComponentThemeCssVar(themeName, "r-base", datas.value),
+    ...getComponentThemeCssVar(themeName, componentsName, datas.value),
   };
 });
 
