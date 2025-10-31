8835bd2726b32ec81ef123e38c298ba086afa753
iRainna <caihonglin@joomon.com>
2025-10-31 15:39:25 +0800

    fix: r-divider

--- uni_modules/r-divider/components/r-divider/r-divider.vue
+++ uni_modules/r-divider/components/r-divider/r-divider.vue
@@ -1,160 +1,165 @@
 <template>
-  <view
-    :class="{
+	<view :class="{
       'r-divider': true,
       'r-divider--dashed': dashed,
       'r-divider--hairline': hairline,
       'r-divider--vertical': vertical,
       ['r-divider--content-' + contentPosition]: $slots.default && !vertical,
-    }"
-    :style="{
+    }" :style="{
       ...getComponentThemeStyle,
       ...customStyle,
-    }"
-    @click="onClick"
-  >
-    <slot v-if="!vertical"></slot>
-  </view>
+    }" @click="onClick">
+		<slot v-if="!vertical"></slot>
+	</view>
 </template>
 <script setup>
-import { computed, inject } from "vue";
-import { getComponentThemeCssVar } from "../themes/index.js";
+	import {
+		computed,
+		inject
+	} from "vue";
+	import {
+		datas
+	} from '../themes/index.js';
+	import {
+		getComponentThemeCssVar
+	} from "@/uni_modules/r-theme-base/js_sdk/useComponentTheme.js";
 
-const emit = defineEmits(["click"]);
-const props = defineProps({
-  // 是否使用虚线
-  dashed: {
-    type: Boolean,
-    default: false,
-  },
-  //   是否使用 0.5px 线
-  hairline: {
-    type: Boolean,
-    default: false,
-  },
-  //   内容位置，可选值为 left right
-  contentPosition: {
-    type: String,
-    default: "center",
-  },
-  //   是否使用垂直
-  vertical: {
-    type: Boolean,
-    default: false,
-  },
-  //自定义样式
-  customStyle: {
-    type: Object,
-    default: () => {},
-  },
-  //   主题名称
-  themeName: {
-    type: String,
-    default: "default",
-  },
-});
+	const emit = defineEmits(["click"]);
+	const props = defineProps({
+		// 是否使用虚线
+		dashed: {
+			type: Boolean,
+			default: false,
+		},
+		//   是否使用 0.5px 线
+		hairline: {
+			type: Boolean,
+			default: false,
+		},
+		//   内容位置，可选值为 left right
+		contentPosition: {
+			type: String,
+			default: "center",
+		},
+		//   是否使用垂直
+		vertical: {
+			type: Boolean,
+			default: false,
+		},
+		//自定义样式
+		customStyle: {
+			type: Object,
+			default: () => {},
+		},
+		//   主题名称
+		themeName: {
+			type: String,
+			default: "default",
+		},
+	});
 
-const componentsName = "r-divider";
+	const componentsName = "r-divider";
 
-const getComponentThemeStyle = computed(() => {
-  let themeName = props.themeName;
+	const getComponentThemeStyle = computed(() => {
+		let themeName = props.themeName;
 
-  if (props.themeName != "default") {
-    //单独设置了组件的 就用单独设置的
-    themeName = props.themeName;
-  }
+		if (props.themeName != "default") {
+			//单独设置了组件的 就用单独设置的
+			themeName = props.themeName;
+		}
 
-  return {
-    ...getComponentThemeCssVar(themeName, "r-base"),
-    ...getComponentThemeCssVar(themeName, componentsName),
-  };
-});
-const onClick = (e) => emit("click", e);
+		return {
+			...getComponentThemeCssVar(themeName, "r-base", datas.value),
+			...getComponentThemeCssVar(themeName, componentsName, datas.value),
+		};
+	});
+	const onClick = (e) => emit("click", e);
 </script>
 <style lang="scss" scoped>
-.r-divider {
-  display: flex;
-  align-items: center;
-  margin: var(--r-divider-margin);
-  color: var(--r-divider-text-color);
-  font-size: var(--r-divider-font-size);
-  line-height: var(--r-divider-line-height);
-  border-color: var(--r-divider-border-color);
-  border-style: solid;
-  border-width: 0;
+	.r-divider {
+		display: flex;
+		align-items: center;
+		margin: var(--r-divider-margin);
+		color: var(--r-divider-text-color);
+		font-size: var(--r-divider-font-size);
+		line-height: var(--r-divider-line-height);
+		border-color: var(--r-divider-border-color);
+		border-style: solid;
+		border-width: 0;
 
-  &::before,
-  &::after {
-    display: block;
-    flex: 1;
-    box-sizing: border-box;
-    height: 1px;
-    border-color: inherit;
-    border-style: inherit;
-    border-width: var(--r-border-width) 0 0;
-  }
+		&::before,
+		&::after {
+			display: block;
+			flex: 1;
+			box-sizing: border-box;
+			height: 1px;
+			border-color: inherit;
+			border-style: inherit;
+			border-width: var(--r-border-width) 0 0;
+		}
 
-  &::before {
-    content: "";
-  }
+		&::before {
+			content: "";
+		}
 
-  &--hairline {
-    &::before,
-    &::after {
-      transform: scaleY(0.5);
-    }
-  }
+		&--hairline {
 
-  &--dashed {
-    border-style: dashed;
-  }
+			&::before,
+			&::after {
+				transform: scaleY(0.5);
+			}
+		}
 
-  &--content-center,
-  &--content-left,
-  &--content-right {
-    &::before {
-      margin-right: var(--r-divider-content-padding);
-    }
+		&--dashed {
+			border-style: dashed;
+		}
 
-    &::after {
-      margin-left: var(--r-divider-content-padding);
-      content: "";
-    }
-  }
+		&--content-center,
+		&--content-left,
+		&--content-right {
+			&::before {
+				margin-right: var(--r-divider-content-padding);
+			}
 
-  &--content-left {
-    &::before {
-      max-width: var(--r-divider-content-left-width);
-    }
-  }
+			&::after {
+				margin-left: var(--r-divider-content-padding);
+				content: "";
+			}
+		}
 
-  &--content-right {
-    &::after {
-      max-width: var(--r-divider-content-right-width);
-    }
-  }
+		&--content-left {
+			&::before {
+				max-width: var(--r-divider-content-left-width);
+			}
+		}
 
-  &--vertical {
-    display: inline-block;
-    width: var(--r-border-width);
-    height: 1em;
-    margin: var(--r-divider-vertical-margin);
-    vertical-align: middle;
+		&--content-right {
+			&::after {
+				max-width: var(--r-divider-content-right-width);
+			}
+		}
 
-    &::before {
-      height: 100%;
-      border-width: 0 0 0 var(--r-border-width);
-    }
+		&--vertical {
+			display: inline-block;
+			width: var(--r-border-width);
+			height: 1em;
+			margin: var(--r-divider-vertical-margin);
+			vertical-align: middle;
 
-    &::after {
-      display: none;
-    }
+			&::before {
+				height: 100%;
+				border-width: 0 0 0 var(--r-border-width);
+			}
 
-    &.r-divider--hairline {
-      &::before {
-        transform: scaleX(0.5);
-      }
-    }
-  }
-}
+			&::after {
+				display: none;
+			}
+
+			&.r-divider--hairline {
+				&::before {
+					transform: scaleX(0.5);
+				}
+			}
+		}
+	}
 </style>
