cb7ea8dc9f62686c33981bcc13001446824494e8
iRainna <1050732924@qq.com>
2025-11-04 16:40:16 +0800

    fix：r-sidebar

---
+++ uni_modules/r-sidebar/components/themes/theme/default/index.js
@@ -0,0 +1,15 @@
+import { ref } from "vue";
+import defaultData from "@/uni_modules/r-theme-base/js_sdk/baseCssVar.js";
+const files = import.meta.glob("./modules/*.js", { eager: true });
+
+const datas = ref(
+  (() => ({
+    ...defaultData.value,
+    ...Object.keys(files).reduce(
+      (pre, key) => ({ ...pre, ...files[key].default?.value }),
+      []
+    ),
+  }))()
+);
+
+export default datas;
